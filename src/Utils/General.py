aedb4275198461d3162a51a181bbe93212e095f8
Parth Parpyani <parpyaniparth@gmail.com>
2021-11-21 14:28:06 -0500

    remove file for load_database, move all utility functions to general

--- src/Utils/General.py
+++ src/Utils/General.py
@@ -4,3 +4,9 @@ def unix_time_to_date_time_string(unix_timestamp):
     # divide unixTimestamp by 1000 as Siginal data timestamp is given in milliseconds
     date_time_string = datetime.fromtimestamp(unix_timestamp/1000).strftime('%Y-%m-%d %H:%M:%S.%f')
     return date_time_string
+
+def are_required_parameters_in_message(message, required_parameters):
+    for parameter in required_parameters:
+        if parameter not in message:
+            return False
+    return True
