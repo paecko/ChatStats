a10974a766bf8de9c362d85bf928e44e6163cc8f
Parth Parpyani <parpyaniparth@gmail.com>
2021-11-21 14:26:48 -0500

    put method for loading database into MessageDatabase class

--- src/MessageDatabase.py
+++ src/MessageDatabase.py
@@ -1,4 +1,7 @@
 import sqlite3
+from Utils.General import are_required_parameters_in_message, unix_time_to_date_time_string
+
+
 REQUIRED_PARAMETERS = ['timestamp', 'sourceUuid', 'body', 'conversationId']
 
 
@@ -14,8 +17,15 @@ class MessageDatabase:
         self.cursor.execute("DROP TABLE IF EXISTS %s" % self.table)
         self.cursor.execute("CREATE TABLE %s (timestamp text, source_uuid TEXT, body TEXT, conversation_id TEXT)" % self.table)
     
+    def load_database_with_json_messages(self, json_obj):
+        for message in json_obj:
+            if are_required_parameters_in_message(message, REQUIRED_PARAMETERS):
+                timestamp_string = unix_time_to_date_time_string(message['timestamp'])
+                extracted_message_data = (timestamp_string, message['sourceUuid'], message['body'], message['conversationId'])
+                self.insert(extracted_message_data)
+        self.close_connection()
 
-    def insert(self, values:list):
+    def insert(self, values:tuple):
         placeholder_values = "?" * len(values)
         placeholder_values = ",".join(placeholder_values)
         insert_statement = """INSERT INTO %s (timestamp, source_uuid, body, conversation_id) VALUES (%s)""" % (self.table, placeholder_values)
@@ -24,6 +34,8 @@ class MessageDatabase:
 
     def close_connection(self):
         self.connection.close()
+
+
 
     ''' general insert statement
     def insert(self, fields:list, values:list):
