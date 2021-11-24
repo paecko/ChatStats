1ed28bc7af553101538f83ca36821106fdc0806f
Parth Parpyani <parpyaniparth@gmail.com>
2021-11-23 19:27:44 -0500

    add conversation_id as parameter

--- src/MessageTable.py
+++ src/MessageTable.py
@@ -1,19 +1,19 @@
 from MessageDatabase import REQUIRED_PARAMETERS
 from Table import Table
 from Utils.General import unix_time_to_date_time_string, are_required_parameters_in_message, create_value_formatters
-from DatabaseCursor import DatabaseCursor
 
-REQUIRED_PARAMETERS = ['timestamp', 'sourceUuid', 'body', 'conversationId']
+REQUIRED_PARAMETERS = ['timestamp', 'sourceUuid', 'body']
 
 class MessageTable(Table):
     def __init__(self, cursor, conversation_id):
-        self.conversation_id = conversation_id
+        super().__init__(cursor, conversation_id)
         self.table_name = "Messages" + self.conversation_id.replace("-", "")
-        Table.__init__(cursor, self.table_name)
+        self.cursor.execute("DROP TABLE IF EXISTS %s" % self.table_name)
         self.cursor.execute("CREATE TABLE %s (timestamp text, source_uuid TEXT, body TEXT)" % self.table_name)
 
     def load_table(self, message=""):
         if are_required_parameters_in_message(message, REQUIRED_PARAMETERS) and message['conversationId'] == self.conversation_id:
             timestamp_string = unix_time_to_date_time_string(message['timestamp'])
-            self.insert_in_table({"timestamp": timestamp_string, "fromId":message['sourceUuid'], "body": message["body"]})
-      
+            self.insert_in_table({"timestamp": timestamp_string, "source_uuid":message['sourceUuid'], "body": message["body"]})
+    
+    
--- src/ReactionsTable.py
+++ src/ReactionsTable.py
@@ -4,14 +4,14 @@ from Table import Table
 
 class ReactionsTable(Table):
     def __init__(self, cursor, conversation_id):
-        self.cursor = cursor
+        super().__init__(cursor, conversation_id)
         self.conversation_id = conversation_id
         self.table_name = "Reactions" + self.conversation_id.replace("-", "")
         self.cursor.execute("DROP TABLE IF EXISTS %s" % self.table_name)
         self.cursor.execute("CREATE TABLE %s (emoji TEXT, fromId TEXT, targetAuthorUuid TEXT)" % self.table_name)
 
     def load_table(self, message):
-        if message['reactions']:
+        if 'reactions' in message:
             for reaction in message['reactions']:
                 self.insert_in_table({"emoji": reaction['emoji'], "fromId":reaction['fromId'], "targetAuthorUuid":reaction['targetAuthorUuid']})
         
