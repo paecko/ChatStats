9671fa419913cd7ab14048e8addfc8f5ded5bc0a
Parth Parpyani <parpyaniparth@gmail.com>
2021-11-23 19:28:11 -0500

    add method for fetchall

--- src/DatabaseCursor.py
+++ src/DatabaseCursor.py
@@ -5,10 +5,13 @@ class DatabaseCursor:
         self.connection = sqlite3.connect(database_name + ".db")
         self.cursor = self.connection.cursor()
 
-    def execute(self, *args):
-        self.cursor.execute(*args)
+    def execute(self, statement, values=None):
+        if values is None:
+            self.cursor.execute(statement)
+        else:
+            self.cursor.execute(statement, values)
+        self.connection.commit()
 
-    def commit(self):
-        self.connection.commit
-
+    def fetchall(self):
+        return self.cursor.fetchall()    
     
