e150216d639dba9c71e5607d572ae687099a3977
Parth Parpyani <parpyaniparth@gmail.com>
2021-11-27 15:43:01 -0500

    example main function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,26 @@
 from SignalJsonMessages import SignalJsonMessages
 from MessageDatabase import MessageDatabase
-from Utils.load_database import load_database
+from MessageTable import MessageTable
+from ReactionsTable import ReactionsTable
+from DatabaseCursor import DatabaseCursor
+from Plotter import Plotter
+from Table import Table
+
+#signalJsonData = SignalJsonMessages("db-2.sqlite", "config.json")
+
+cursor = DatabaseCursor("Final")
+
+messageTable = MessageTable(cursor, "456f3ffe-eedc-42d1-9242-741be78fe5c7", create_new = False)
+reactionTable = ReactionsTable(cursor, "456f3ffe-eedc-42d1-9242-741be78fe5c7", create_new=False)
+
+#Table.load_json_in_tables(signalJsonData, messageTable)
+
+reactionTable.set_users(csv_file="Users.csv")
+messageTable.set_users(csv_file="Users.csv")
+print(reactionTable.plot_receieved_reactions_percentages(messageTable))
+#plt = Plotter(reactiontable, None, "Users.csv")
+#plt.plot_most_popular_emojis()
 
 
-signalJsonData = SignalJsonMessages("db.sqlite", "config.json")
-Database = MessageDatabase()
-load_database(Database, signalJsonData)
+
+
