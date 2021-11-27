94f2db59c023d52520dd047e3f68c6306bde062b
Parth Parpyani <parpyaniparth@gmail.com>
2021-11-27 15:41:38 -0500

    Add method for getting messages per user

--- src/MessageTable.py
+++ src/MessageTable.py
@@ -1,6 +1,7 @@
 from MessageDatabase import REQUIRED_PARAMETERS
 from Table import Table
 from Utils.General import unix_time_to_date_time_string, are_required_parameters_in_message
+from string import Template
 
 REQUIRED_PARAMETERS = ['timestamp', 'sourceUuid', 'body', 'conversationId']
 
@@ -17,4 +18,11 @@ class MessageTable(Table):
             timestamp_string = unix_time_to_date_time_string(message['timestamp'])
             self.insert_in_table({"timestamp": timestamp_string, "source_uuid":message['sourceUuid'], "body": message["body"]})
     
-    
+    def get_messages_per_user(self):
+        query_template = Template("SELECT source_uuid, COUNT() as count FROM $table_name GROUP BY source_uuid ORDER BY source_uuid")
+        results = self.query_table(query_template)
+        counts = []
+        for res in results:
+            if res[0] in self.uuid_to_name:
+                counts.append(res[1])
+        return counts 
