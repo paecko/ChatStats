852c01d7c5c769398af0cf60c9a1526cc4502d60
Parth Parpyani <parpyaniparth@gmail.com>
2021-11-27 15:42:34 -0500

    Added function for getting percentage of reactions over total messages

--- src/ReactionsTable.py
+++ src/ReactionsTable.py
@@ -21,14 +21,29 @@ class ReactionsTable(Table):
             for reaction in message['reactions']:
                 self.insert_in_table({"emoji": reaction['emoji'], "fromId":reaction['fromId'], "targetAuthorUuid":reaction['targetAuthorUuid']})
 
-    def initialize_emoji_counts(self, key_emojis):
-        # create dict that maps each emoji to a count of 0
-        zero_count_emoji_dict = {}
-        for emoji in key_emojis:
-            zero_count_emoji_dict[emoji] = 0
-        # looks like {👍: 0, 😮: 0, ...}
-        return zero_count_emoji_dict
 
+    def plot_receieved_reactions_percentages(self, message_table):
+        self.plot_received_user_reactions(message_table)
+
+    def plot_received_user_reactions(self, message_table = None):
+        key_emojis, count = self.get_most_popular_emojis()
+        uuid_to_emoji_counts = self.populate_reaction_counts(key_emojis)
+        emoji_counts = self.create_transposed_emoji_counts_list(uuid_to_emoji_counts)
+        if message_table is not None:
+            self.reaction_percentage_over_total_messages(message_table, emoji_counts)
+
+        data_frame = self.create_reaction_dataframe(key_emojis, emoji_counts)
+        names = self.get_names_list()
+        df = pd.DataFrame(data_frame, index=names)
+        ax = df.plot.bar(rot=0)
+        plt.show()
+
+    def create_reaction_dataframe(self, key_emojis, emoji_counts):
+        data_frame = {}
+        for i in range(len(key_emojis)):
+            data_frame[key_emojis[i]] = emoji_counts[i]
+        return data_frame
+    
     def populate_reaction_counts(self, key_emojis):
         query_template = Template("SELECT targetAuthorUuid, emoji, COUNT(*) as count FROM $table_name \
             GROUP BY emoji, targetAuthorUuid ORDER BY targetAuthorUuid, emoji")
@@ -44,29 +59,24 @@ class ReactionsTable(Table):
                 uuid_to_emoji_counts[uuid][emoji] = count                
         return uuid_to_emoji_counts
 
-    def create_reaction_dataframe(self):
-        key_emojis, count = self.get_most_popular_emojis()
-        uuid_to_emoji_counts = self.populate_reaction_counts(key_emojis)
-        emoji_counts_as_lists = []
+    def initialize_emoji_counts(self, key_emojis):
+        # create dict that maps each emoji to a count of 0
+        zero_count_emoji_dict = {}
+        for emoji in key_emojis:
+            zero_count_emoji_dict[emoji] = 0
+        # looks like {👍: 0, 😮: 0, ...}
+        return zero_count_emoji_dict
 
+    def create_transposed_emoji_counts_list(self, uuid_to_emoji_counts):
+        emoji_counts_as_lists = []
         for uuid in uuid_to_emoji_counts:
             emoji_count_list = list(uuid_to_emoji_counts[uuid].values())
             emoji_counts_as_lists.append(emoji_count_list)
         # transpose list so each subarray represents the counts for 1 emoji for every person. 
         # Rather than each subarray representing the counts for each emoji per person.
         emoji_counts = list(map(list, zip(*emoji_counts_as_lists)))
+        return emoji_counts
 
-        data_frame = {}
-        for i in range(len(key_emojis)):
-            data_frame[key_emojis[i]] = emoji_counts[i]
-        return data_frame
-
-    def plot_received_user_reactions(self):
-        data_frame_dict = self.create_reaction_dataframe()
-        names = self.get_names_list()
-        df = pd.DataFrame(data_frame_dict, index=names)
-        ax = df.plot.bar(rot=0)
-        plt.show()
 
     def get_most_popular_emojis(self):
         query_template = Template("SELECT emoji, COUNT(*) FROM $table_name GROUP BY emoji")
@@ -76,4 +86,18 @@ class ReactionsTable(Table):
         emojis = [t[0] for t in results]
         counts = [t[1] for t in results]
         return emojis, counts
+
+    def reaction_percentage_over_total_messages(self, message_table, emoji_counts):
+        messages_per_user = message_table.get_messages_per_user()
+        for emoji_count_list in emoji_counts:
+            for i in range(len(emoji_count_list)):
+                if messages_per_user[i] != 0:
+                    emoji_count_list[i] = (emoji_count_list[i]/messages_per_user[i]) * 100
+                else:
+                    emoji_count_list[i] = 0
+        return emoji_count_list
+
+
+
+        
         
