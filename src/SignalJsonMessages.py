61df8516fc8593f1b628d06032905e13b682ac95
Parth Parpyani <30911068+paecko@users.noreply.github.com>
2021-12-04 13:24:15 -0500

    Allow for loading multiple sqlite databases.

--- src/SignalJsonMessages.py
+++ src/SignalJsonMessages.py
@@ -3,15 +3,24 @@ import json
 
 
 class SignalJsonMessages:
-    def __new__(cls, sqlite_path, config_path, create_json_file=False):
+
+    def __new__(cls, config_to_sqlite_paths: dict, create_json_file=False):
+        json_message_objects = []
+        for config, sqlite in config_to_sqlite_paths.items():
+            SignalJsonMessages.run_sqlite_to_json_script(config, sqlite, json_message_objects)
+
+        if create_json_file:
+            with open('data.json', 'w', encoding='utf-8') as f:
+                json.dump(json_message_objects, f, ensure_ascii=False, indent=4)
+                
+        return json_message_objects
+
+    @classmethod
+    def run_sqlite_to_json_script(cls, config_path, sqlite_path, json_message_objects):
         json_str_in_bytes = subprocess.check_output(["sh", "./sqlite-to-json.sh", sqlite_path, config_path])
         json_str_list = json.loads(json_str_in_bytes)
         # last item in returned list is an empty string so we remove it.
         if len(json_str_list) > 0:
             json_str_list.pop()
-        json_message_objects = []
-
         for message in json_str_list:
             json_message_objects.append(json.loads(message))
-
-        return json_message_objects
