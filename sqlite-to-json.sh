5f47f0bddcadbd8a81783b388a99106a6163a027
Parth Parpyani <30911068+paecko@users.noreply.github.com>
2021-12-22 21:48:45 -0500

    Delete sqlite-to-json.sh

--- sqlite-to-json.sh
+++
@@ -1,4 +0,0 @@
-db="$1";
-key=$( /usr/local/bin/jq -r '."key"' $2 );
-
-/usr/local/bin/sqlcipher -list -noheader "$db" "PRAGMA key = \"x'"$key"'\";select json from messages;" | tail -n +2 | jq -R -s -c 'split("\n")'
